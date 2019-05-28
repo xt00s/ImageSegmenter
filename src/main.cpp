475f54bc6b07f5cf03481efca1b7f94122626936
xtoos <xtoos.rri@gmail.com>
2019-05-28 18:36:44 +0300

    hide question mark help button for dialogs

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
+    QApplication a(argc, argv);
+    QApplication::setAttribute(Qt::AA_DisableWindowContextHelpButton);
 	MainWindow w;
 	w.show();
-	return a.exec();
+    return QApplication::exec();
 }
